7c32f13bf737ba3290d74d0f8a2f6c3e53c47063
Michael Matuzak <matuzak@yahoo-inc.com>
2014-02-21 11:08:53 -0800

    first

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+/*jslint node: true, nomen: true, vars: true */
 var LRU = require('lru-cache');
+var Ycb = require('ycb').Ycb;
+var LRU = require('lru-cache');
+var options = {
+    max: 200
+};
+
+function YcbLru(config) {
+    if (!(this instanceof YcbLru)) {
+        return new YcbLru(config);
+    }
+
+    this.cache = LRU(options);
+    this.ycb = new Ycb(config, {});
+}
+
+YcbLru.prototype.read = function (context) {
+    var key = JSON.stringify(context);
+    var cache = this.cache;
+    var config;
+
+    if (cache.has(key)) {
+        config = cache.get(key);
+    } else {
+        config = this.ycb.read(context);
+        cache.set(key, config);
+    }
+    return config;
+};
+
+module.exports = YcbLru;
---
+++ tests/basic.js
@@ -0,0 +1,23 @@
+var test = require('tap').test;
+var Ycb = require('../');
+
+test('read config', function (t) {
+    var dimensions = require('./dimensions');
+    var appConfig = require('./application');
+    var settings = dimensions.concat(appConfig);
+    var ycb = Ycb(settings);
+
+    var config = ycb.read({});
+    t.equal(8666, config.appPort);
+
+    config = ycb.read({environment: 'prod'});
+    t.equal(80, config.appPort);
+
+    config = ycb.read({device: 'desktop'});
+    t.equal(8080, config.appPort);
+
+    config = ycb.read({environment: 'prod', device: 'smartphone'});
+    t.equal(8888, config.appPort);
+
+    t.end();
+});
